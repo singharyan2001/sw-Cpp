c5af6b7022c4486a0e19c4ca91808f5081a40088
singharyan2001 <singharyan2001@gmail.com>
2026-07-17 19:11:48 +0530

    completed auto keyword trials and doc, now starting templates in C++

---
+++ Cpp028_templates/src/main.cpp
@@ -0,0 +1,9 @@
+// Templates in C++
+#include <iostream>
+
+int main(){
+    std::cout << "============ TOPIC: TEMPLATES IN C++ ============" << std::endl;
+
+    std::cout << "=================================================" << std::endl;
+    std::cin.get();
+}
